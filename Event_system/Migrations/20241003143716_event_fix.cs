648a9d3dd660a3c19abdabf1aa8ec89a8d7a5137
aivmic <aivarasnori@gmail.com>
2024-10-03 18:19:57 +0300

    added OpenApi specification

---
+++ Event_system/Migrations/20241003143716_event_fix.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class event_fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Events",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Events",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
